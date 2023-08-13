ff33fba2ff6c2b71bfbc1eb0b687d48494516948
Alexander <sahaal2019@mail.ru>
2023-08-13 09:28:35 +0400

    my

--- sql Insert1.sql
+++ sql Insert1.sql
@@ -2,15 +2,15 @@ INSERT INTO Genre(Name) values('Кантри'), ('Блюз'), ('Фолк');
 
 insert INTO Performers(Name) values('Шевчук'), ('Шнур Шнурович'), ('Лепс'), ('Кипелов');
 
-INSERT into Performers_and_Genre (Genre, Performers) values ('Кантри', 'Шевчук'), ('Блюз', 'Шнур Шнурович'), ('Фолк', 'Лепс'), ('Кантри', 'Шевчук'); 
+INSERT into Performers_and_Genre (Genre, Performers) values (1, 1), (2, 2), (3, 3), (3, 4); 
 
 insert into Album(Name, release) values ('Rumours', 2020), ('Saturday Night Fever', 2020), ('Millennium', 2015);
 
-insert  into Performers_and_Album (Performers, Album) values ('Шевчук', 'Rumours'), ('Шнур Шнурович', 'Saturday Night Fever'), ('Лепс', 'Millennium'), ('Кипелов','Millennium' );
+insert  into Performers_and_Album (Performers, Album) values (1, 1), (2, 2), (3, 3), (4, 3);
 
 insert into Tracks(Name, Duration, Exit) values ('мой', '00:04:00', 2019), ('Б', '00:02:30', 2022), ('С', '00:09:00', 2018), ('В', '00:08:00', 2020), ('Т', '00:06:00', 2020), ('Н', '00:02:58', 2018);
 
 insert into Collection(Name, release) values ('Про любовь', 2019), ('День рождения', 2010), ('Свадебные', 2020), ('Медляки', 2018);
 
-insert into Collection_and_Tracks (Collection, Tracks) values ('Про любовь', 'мой'), ('День рождения', ('Б', 'C')), ('Свадебные', 'В'), ('Медляки', ('Т', 'Н'));
+insert into Collection_and_Tracks (Collection, Tracks) values (1, 1), (2, 2), (3, 3), (4, 4), (4, 5), (4, 6);
 
